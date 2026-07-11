74a5b18af9c0d513fe5eb2fcbd95cd9c7c829728
RITIKA RAI <workdoc24.r@gmail.com>
2026-07-11 23:55:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def analyze_symptoms_with_groq(text_input):
                 {"role": "system", "content": system_instruction},
                 {"role": "user", "content": f"Analyze these patient observations: {text_input}"}
             ],
-            model="llama-3.1-8b-instant", 
+            model="llama-3.3-70b-versatile", 
             temperature=0.1, # Low temperature ensures focused, analytical outputs
             max_tokens=150
         )
